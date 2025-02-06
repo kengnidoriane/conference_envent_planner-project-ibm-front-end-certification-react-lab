f1891be56db82ae7523ef2477845aca6f62b785b
unknown <dorianekengni@gmail.com>
2025-02-06 17:16:31 +0200

    add logic to calculate avTotal cost, declare variable to assign a value and put this variable in total addons button

--- src/ConferenceEvent.jsx
+++ src/ConferenceEvent.jsx
@@ -59,9 +59,15 @@ const ConferenceEvent = () => {
             totalCost += item.cost * item.quantity;
           });
         }
+        else if (section === "av") {
+          avItems.forEach(item => {
+            totalCost += item.cost * item.quantity;
+          })
+        }
         return totalCost;
       };
     const venueTotalCost = calculateTotalCost("venue");
+    const avTotalCost = calculateTotalCost("av");
 
     const navigateToProducts = (idType) => {
         if (idType == '#venue' || idType == '#addons' || idType == '#meals') {
@@ -177,7 +183,7 @@ const ConferenceEvent = () => {
                                       </div>
                                   ))}
                                 </div>
-                                <div className="total_cost">Total Cost:</div>
+                                <div className="total_cost">Total Cost: ${avTotalCost}</div>
 
                             </div>
 
